f6fc4ccef375e2fea2b0a106a98eed51287f6182
Raffi Dilanchian <raffidilanchian@gmail.com>
2023-10-09 22:25:04 +0200

    Add pace to reports with r=2

--- rlnc/main.py
+++ rlnc/main.py
@@ -4,14 +4,14 @@ from block_based_rlnc import BlockBasedRLNC
 
 base_config = dict(field_order=2**8, generation_size=16,
                    packet_size=16, total_size=1024*32,
-                   initial_redundancy=4, initial_window_size=1,
+                   initial_redundancy=2, initial_window_size=1,
                    ge_loss_good_to_bad=0.05, ge_loss_bad_to_good=0.2,
                    exponential_loss_param=0.045,
                    ee_loss_error=0.10, seed=24,
                    force_to_recreate_new_data=True)
 
 
-config = dict(loss_mode="constant", loss_rate=0.2, approach="standard",)
+config = dict(loss_mode="constant", loss_rate=0.2, approach="pace",)
 
 applied_config = base_config | config
 
